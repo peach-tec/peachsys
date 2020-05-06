3b0d423a83e906e808e303ca32195c0499fab5c1
peach-tec <18482133793@163.com>
2020-05-06 21:23:04 +0800

    首次新建项目

---
+++ src/main/java/com/dyh/peachsys/PeachsysApplication.java
@@ -0,0 +1,13 @@
+package com.dyh.peachsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeachsysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeachsysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dyh/peachsys/PeachsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyh.peachsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeachsysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
