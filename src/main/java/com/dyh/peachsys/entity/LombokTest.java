331445761463e69f6c06707d395225248d5f8d3b
peach-tec <18482133793@163.com>
2020-05-07 18:51:43 +0800

    增加Lombok的jar包，演示Lombok的使用

---
+++ src/main/java/com/dyh/peachsys/entity/LombokTest.java
@@ -0,0 +1,18 @@
+package com.dyh.peachsys.entity;
+
+import lombok.Data;
+
+/**
+ * @author Peach-華
+ * @version V1.0
+ * @desc    Lombok测试实体类演示
+ * @copyright &copy; DYH
+ * @date 2020/5/7-18:38
+ * @address 成都
+ */
+@Data
+public class LombokTest {
+    private Integer id;
+    private String username;
+    private String password;
+}
