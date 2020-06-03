88403be0d51e6530583298fd26f758c413ae4e28
peach-tec <18482133793@163.com>
2020-06-03 17:00:15 +0800

    优化ueditor配置,通过io流读取配置，然后将文件保存到指定位置

--- src/main/java/com/dyh/peachsys/api/UEditorApi.java
+++ src/main/java/com/dyh/peachsys/api/UEditorApi.java
@@ -1,15 +1,15 @@
 package com.dyh.peachsys.api;
 
-import com.baidu.ueditor.ActionEnter;
-import org.springframework.util.ClassUtils;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
 /**
@@ -23,30 +23,66 @@ import java.io.PrintWriter;
 @RestController
 @RequestMapping("/")
 public class UEditorApi {
+    /**
+     * 通过后端跳转到主页
+     *
+     * @return
+     */
     public String home() {
         return "index";
     }
 
+    /**
+     * uedtior的配置和上传
+     *
+     * @param request
+     * @param response
+     * @param action   需要执行的操作，通过config.json中的imageActionName属性来执行，如果action=config，则表示执行读取config.json内容
+     * @param upfile   需要上传的文件
+     * @throws IOException
+     */
     @RequestMapping("/config")
     public void config(HttpServletRequest request, HttpServletResponse response, String action, MultipartFile upfile) throws IOException {
-        System.out.println("正在保存文件");
-        String path = "/image/20200529/1590762943597055974.jpg";
+        String path = null;
         request.setCharacterEncoding("utf-8");
         response.setContentType("application/json");
-//        response.setHeader("Content-Type", "text/html");
-//        if (action.equals("config")){
-            //获取config.json所在的目录
-            String rootPath = ClassUtils.getDefaultClassLoader().getResource("").getPath()+"static";
-            path = new ActionEnter(request, rootPath).exec();
-//        }
+        //获取config.json的路径
+        String rootPath = "src/main/resources/static";
+        if ("config".equals(action)) {
+            //将config.json放在resources根目录下，用io流来读取
+            //因为在导成jar包部署后获取不到config.json的路径
+            ClassPathResource resource = new ClassPathResource("config.json");
+            StringBuffer buffer = new StringBuffer();
+            try {
+                InputStreamReader reader = new InputStreamReader(resource.getInputStream(), "UTF-8");
+                BufferedReader bf = new BufferedReader(reader);
+                String str = null;
+                while ((str = bf.readLine()) != null) {
+                    buffer.append(str);
+                }
+                path = buffer.toString().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            //可以改成调用自己的保存方法，然后返回json格式的字符串，示例：
+            String testPath = "/image/20200603/1591154731689075892.jpg";//引用我本地的图片
+            //这是返回的数据，需要将url字段的值改为testPath
+            long size = upfile.getSize();
+            String originalFilename = upfile.getOriginalFilename();
+            String type = upfile.getOriginalFilename().substring(upfile.getOriginalFilename().lastIndexOf("."));
+            String title = testPath.substring(testPath.lastIndexOf("/") + 1);
+            path = "{\"state\": \"SUCCESS\"," +
+                    "\"original\": \"" + originalFilename + "\"," +
+                    "\"size\": \"" + size + "\"," +
+                    "\"title\": \"" + title + "\"," +
+                    "\"type\": \"" + type + "\"," +
+                    "\"url\": \"" + testPath + "\"}";
+        }
+        //将返回的路径返回至ueditor
         PrintWriter writer = response.getWriter();
         writer.write(path);
         writer.flush();
         writer.close();
-    }
-
-    @PostMapping("/post")
-    public void post(){
-
     }
 }
