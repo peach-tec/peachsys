e70a79e2d1d14ab801b4eeeb8b5717fc595e088a
peach-tec <18482133793@163.com>
2020-07-04 21:37:11 +0800

    新增文件下载功能

--- src/main/java/com/dyh/peachsys/file/controller/FileApi.java
+++ src/main/java/com/dyh/peachsys/file/controller/FileApi.java
@@ -1,14 +1,16 @@
 package com.dyh.peachsys.file.controller;
 
 import com.dyh.peachsys.peach.common.Res;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URLEncoder;
 
 /**
  * @author Peach-華
@@ -30,10 +32,38 @@ public class FileApi {
             return Res.fail("上传文件不能为空");
         }
 
-        File dest = new File(path + fileName);
 
-        file.transferTo(dest);
+//        File dest = new File(path + fileName);
+
+//        file.transferTo(dest);
 
         return Res.success(path + fileName);
+    }
+
+    @GetMapping("/download")
+    public Res download(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String path = "D:/filetest";
+        String fileName = "logon.jpg";
+
+        response.reset();
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("multipart/form-data");
+        response.setHeader("Content-Disposition", "attachment;fileName=" + URLEncoder.encode(fileName, "UTF-8"));
+
+        File file = new File(path, fileName);
+        //读取文件输入流
+        FileInputStream input = new FileInputStream(file);
+        //写出文件输出流
+        ServletOutputStream out = response.getOutputStream();
+        byte[] buff = new byte[1024];
+        int index = 0;
+        while ((index = input.read(buff)) != -1) {
+            out.write(buff, 0, index);
+            out.flush();
+        }
+        out.close();
+        input.close();
+
+        return Res.success();
     }
 }
